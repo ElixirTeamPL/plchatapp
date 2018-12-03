815ed2d3068952a0cd3bae1e9caa38d0f793d666
John Zubiri <isjohnz@gmail.com>
2018-12-02 21:48:22 -0800

    fixes

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Chat.Mixfile do
     [
       app: :chat,
       version: "1.4.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.5.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,

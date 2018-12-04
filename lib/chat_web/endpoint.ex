8948b5767598fbb55469f20cd9941b16c700c8e6
seanfcastillo <seanfcastillo@gmail.com>
2018-12-03 19:24:32 -0800

    changed from only to only_matching in endpoint

--- lib/chat_web/endpoint.ex
+++ lib/chat_web/endpoint.ex
@@ -13,7 +13,7 @@ defmodule ChatWeb.Endpoint do
     at: "/",
     from: :chat,
     gzip: true,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only_matching: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
